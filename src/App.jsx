e8537665a46995805ea6904d8adea551fc1f720c
sam <a0983131803@gmail.com>
2023-08-15 21:09:38 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Signup from "./components/Signup"
 
 function App() {
 
   return (
     <>
-      
+     
+      <BrowserRouter> 
+      <Navbar/>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
